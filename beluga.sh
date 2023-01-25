344f72cbf63a6c1497cb7241f56501712ad4a707
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-01-25 15:43:40 -0700

    mujoco

---
+++ beluga.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=2  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=1200M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-72:00
+#SBATCH --output=%N-%j-naive.out
+#SBATCH --account=def-ashique
+
+source $HOME/Documents/ENV/bin/activate
+module load python/3.10
+module load mujoco mpi4py
+
+SECONDS=0
+
+python Hyperparam/weighted_ppo_tune.py --seed 189 --log_dir $SCRATCH/avg_discount/ --env 'InvertedDoublePendulum-v4' --epochs 500 &
+python Hyperparam/naive_ppo_tune.py --seed 2 --log_dir $SCRATCH/avg_discount/ --env 'HalfCheetah-v4' --epochs 500 &
+echo "Baseline job $seed took $SECONDS"
+sleep 72h
