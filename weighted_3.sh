7c894913dbab197c489c46c6fb017ee1aba1212a
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-01-25 16:06:26 -0700

    mujoco

--- weighted_3.sh
+++ weighted_3.sh
@@ -5,13 +5,13 @@
 #SBATCH --output=%N-%j-naive.out
 #SBATCH --account=def-ashique
 
-source $HOME/Documents/ENV/bin/activate
 module load python/3.10
+source $HOME/Documents/ENV/bin/activate
 module load mujoco mpi4py
 
 SECONDS=0
-python Hyperparam/weighted_ppo_tune.py --seed 189 --log_dir $SCRATCH/avg_discount/ --env 'Walker2d-v4' --epochs 500 &
-python Hyperparam/weighted_ppo_tune.py --seed 189 --log_dir $SCRATCH/avg_discount/ --env 'HalfCheetah-v4' --epochs 500 &
+python Hyperparam/weighted_ppo_tune.py --seed 107 --log_dir $SCRATCH/avg_discount/logs/ --env 'Walker2d-v4' --epochs 500 &
+python Hyperparam/weighted_ppo_tune.py --seed 107 --log_dir $SCRATCH/avg_discount/logs/ --env 'HalfCheetah-v4' --epochs 500 &
 #python Hyperparam/naive_ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/ --env 'Ant-v4' --epochs 500 &
 echo "Baseline job $seed took $SECONDS"
 sleep 72h
