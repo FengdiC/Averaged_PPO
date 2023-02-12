6be902210b2f0a61023cc20bd3514ab171f5419c
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-02-12 11:20:42 -0700

    tune

--- Hyperparam/ppo_tune.py
+++ Hyperparam/ppo_tune.py
@@ -10,41 +10,32 @@ from Components import logger
 import itertools
 import numpy as np
 import gym
-
-param = {'target_kl':[0.06,0.03,0.01],'pi_lr':[6e-4,3e-4,1e-4]}
+from random_search import random_search,set_one_thread
 
 args = argsparser()
-seeds = range(3)
+seeds = range(10)
 
-logger.configure('./data', ['csv'], log_suffix='Hopper-naive-ppo-tune')
+# Torch Shenanigans fix
+set_one_thread()
 
-for values in list(itertools.product( param['target_kl'], param['pi_lr'])):
-    returns = []
-    for seed in seeds:
-        args.seed = seed
+logger.configure(args.log_dir, ['csv'], log_suffix='Ant-biased-ppo-tune-'+str(args.env))
 
-        checkpoint = 4000
-        result = ppo(lambda: gym.make(args.env), actor_critic=core.MLPActorCritic,
-        ac_kwargs=dict(hidden_sizes=args.hid), gamma=args.gamma,
-        seed=seed, steps_per_epoch=args.steps, epochs=args.epochs,
-        target_kl=values[0],pi_lr = values[1],naive=True)
+returns = []
+for seed in seeds:
+    hyperparam = random_search(args.seed)
+    checkpoint = 4000
+    result = ppo(lambda: gym.make(args.env), actor_critic=core.MLPActorCritic,
+                ac_kwargs=dict(hidden_sizes=args.hid),gamma=hyperparam['gamma'],pi_lr = hyperparam["pi_lr"],
+                target_kl=hyperparam['target_kl'],vf_lr=hyperparam['vf_lr'],epochs=args.epochs,
+                seed=seed,naive=False)
 
-        ret = np.array(result)
-        print(ret.shape)
-        returns.append(ret)
-        name = [str(k) for k in values]
-        name.append(str(seed))
-        print("hyperparam", '-'.join(name))
-        logger.logkv("hyperparam", '-'.join(name))
-        for n in range(ret.shape[0]):
-            logger.logkv(str((n + 1) * checkpoint), ret[n])
-        logger.dumpkvs()
-
-    ret = np.array(returns)
+    ret = np.array(result)
     print(ret.shape)
-    ret = np.mean(ret, axis=0)
-    name = [str(k) for k in values]
-    name.append('mean')
+    returns.append(ret)
+    name = list(hyperparam.values())
+    name = [str(s) for s in name]
+    name.append(str(seed))
+    print("hyperparam", '-'.join(name))
     logger.logkv("hyperparam", '-'.join(name))
     for n in range(ret.shape[0]):
         logger.logkv(str((n + 1) * checkpoint), ret[n])
---
+++ hopper_ppo_biased.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=3  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=1200M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-72:00
+#SBATCH --output=%N-%j.out
+#SBATCH --account=def-ashique
+
+module load python/3.10
+source $HOME/Documents/ENV/bin/activate
+module load mujoco mpi4py
+
+SECONDS=0
+python Hyperparam/ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/ --env 'Ant-v4' --epochs 500&
+
+echo "Baseline job $seed took $SECONDS"
+sleep 72h
