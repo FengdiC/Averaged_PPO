63a554dc8b734469267406af85f358daa313cd05
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-02-12 11:27:35 -0700

    tune

--- hopper_ppo_biased.sh
+++ hopper_ppo_biased.sh
@@ -10,7 +10,7 @@ source $HOME/Documents/ENV/bin/activate
 module load mujoco mpi4py
 
 SECONDS=0
-python Hyperparam/ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/ --env 'Ant-v4' --epochs 500&
+python Hyperparam/ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/ --env 'Hopper-v4' --epochs 500&
 
 echo "Baseline job $seed took $SECONDS"
 sleep 72h
