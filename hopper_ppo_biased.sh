64ee7a3ef8ca8db5485e89765437f9b1da8d55e3
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-02-12 11:41:27 -0700

    tune

--- hopper_ppo_biased.sh
+++ hopper_ppo_biased.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=3  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=1200M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-72:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=def-ashique
+#SBATCH --array=1-500
 
 module load python/3.10
 source $HOME/Documents/ENV/bin/activate
 module load mujoco mpi4py
 
 SECONDS=0
-python Hyperparam/ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/ --env 'Hopper-v4' --epochs 500&
+python Hyperparam/ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/hopper/ --env 'Hopper-v4' --epochs 500&
 
 echo "Baseline job $seed took $SECONDS"
 sleep 72h
--- hopper_ppo_weighted.sh
+++ hopper_ppo_weighted.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=3  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=1200M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-72:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=def-ashique
+#SBATCH --array=1-500
 
-module load python/3.10
 source $HOME/Documents/ENV/bin/activate
+module load python/3.10
 module load mujoco mpi4py
 
 SECONDS=0
-python Hyperparam/weighted_ppo_tune.py --seed 107 --log_dir $SCRATCH/avg_discount/logs --env 'Hopper-v4' --epochs 500 &
-python Hyperparam/weighted_ppo_tune.py --seed 189 --log_dir $SCRATCH/avg_discount/logs --env 'Swimmer-v4' --epochs 500 &
-python Hyperparam/weighted_ppo_tune.py --seed 189 --log_dir $SCRATCH/avg_discount/logs --env 'Reacher-v4' --epochs 500 &
-#python Hyperparam/weighted_ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/ --env 'Ant-v4' --epochs 500&
+#python Hyperparam/weighted_ppo_tune.py --seed 107 --log_dir $SCRATCH/avg_discount/logs --env 'Hopper-v4' --epochs 500 &
+#python Hyperparam/weighted_ppo_tune.py --seed 189 --log_dir $SCRATCH/avg_discount/logs --env 'Swimmer-v4' --epochs 500 &
+#python Hyperparam/weighted_ppo_tune.py --seed 189 --log_dir $SCRATCH/avg_discount/logs --env 'Reacher-v4' --epochs 500 &
+python Hyperparam/weighted_ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/walker/ --env 'Walker2d-v4' --epochs 500&
 
 echo "Baseline job $seed took $SECONDS"
 sleep 72h
