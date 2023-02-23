b3d517188004da62cdbb0ea3e634d0d3f8e6a835
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-02-23 13:15:37 -0700

    tube

--- hopper_ppo_biased.sh
+++ hopper_ppo_biased.sh
@@ -6,12 +6,12 @@
 #SBATCH --account=def-ashique
 #SBATCH --array=1-900
 
-source $HOME/Documents/ENV/bin/activate
 module load python/3.10
+source $HOME/Documents/ENV/bin/activate
 module load mujoco mpi4py
 
 SECONDS=0
-python Hyperparam/ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/halfcheetah/ --env 'HalfCheetah-v4' --epochs 500&
+python Hyperparam/ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/swimmer/ --env 'Swimmer-v4' --epochs 500&
 
 echo "Baseline job $seed took $SECONDS"
 sleep 72h
