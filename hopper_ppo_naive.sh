a3bfdfbe20688cf97986971587126d365a8d222f
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-02-28 21:14:44 -0700

    tube

--- hopper_ppo_naive.sh
+++ hopper_ppo_naive.sh
@@ -6,14 +6,14 @@
 #SBATCH --account=def-ashique
 #SBATCH --array=1-900
 
-module load python/3.10
 source $HOME/Documents/ENV/bin/activate
+module load python/3.10
 module load mujoco mpi4py
 
 SECONDS=0
 #python Hyperparam/naive_ppo_tune.py --seed 2 --log_dir $SCRATCH/avg_discount/ --env 'Hopper-v4' --epochs 500 &
 #python Hyperparam/naive_ppo_tune.py --seed 2 --log_dir $SCRATCH/avg_discount/ --env 'Swimmer-v4' --epochs 500 &
 #python Hyperparam/naive_ppo_tune.py --seed 2 --log_dir $SCRATCH/avg_discount/ --env 'Reacher-v4' --epochs 500 &
-python Hyperparam/naive_ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/halfcheetah/ --env 'HalfCheetah-v4' --epochs 500 &
+python Hyperparam/naive_ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --log_dir $SCRATCH/avg_discount/ant/ --env 'Ant-v4' --epochs 500 &
 echo "Baseline job $seed took $SECONDS"
 sleep 72h
