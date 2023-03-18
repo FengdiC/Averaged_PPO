8f8700852ed17dc1b22ee3e0b74eb0162f49d626
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-03-18 13:01:33 -0600

    dm_control

---
+++ dm_control/naive.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=800M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-72:00
+#SBATCH --output=%N-%j.out
+#SBATCH --account=def-ashique
+
+module load python/3.10
+source $HOME/Documents/ENV/bin/activate
+module load mujoco mpi4py
+module load dm_control
+
+SECONDS=0
+python Reacher_naive.py --seed 790 --type "swingup_sparse" --log_dir $SCRATCH/avg_discount/dm_control/ --epochs 500&
+python Reacher_naive.py --seed 750 --type "swingup_sparse" --log_dir $SCRATCH/avg_discount/dm_control/ --epochs 500&
+python Reacher_naive.py --seed 535 --type "swingup_sparse" --log_dir $SCRATCH/avg_discount/dm_control/ --epochs 500&
+
+echo "Baseline job $seed took $SECONDS"
+sleep 72h
