b08c9c4c0a87321e0e9199d65988faf74be896b7
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-05-19 12:09:39 -0600

    dm

--- Analysis/mountainCar_initial_test.py
+++ Analysis/mountainCar_initial_test.py
@@ -480,10 +480,10 @@ def weighted_ppo(env_fn, actor_critic=core.MLPWeightedActorCritic, ac_kwargs=dic
     buf = PPOBuffer(obs_dim, act_dim, local_steps_per_epoch, gamma, lam)
 
     # discretize the state space to estimate state distributions
-    bins = 70
+    bins = 9
     # but we only study three dimension of states
     n = env.observation_space.shape[0]
-    if n>2:
+    if n>3:
         a = np.arange(n)
         np.random.shuffle(a)
         dim = a[:2]
