96d8a8506d3fcc45c2a4d81f0eebd7f286cdfd32
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2023-05-22 15:48:24 -0600

    dm

--- dm_control/biased.sh
+++ dm_control/biased.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=1200M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-24:00
+#SBATCH --time=0-72:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=def-ashique
 #SBATCH --array=1-250
 
-source $HOME/Documents/ENV/bin/activate
 module load python/3.10
+source $HOME/Documents/ENV/bin/activate
 module load mujoco mpi4py
 
 SECONDS=0
--- dm_control/dm_diff_dist.sh
+++ dm_control/dm_diff_dist.sh
@@ -4,15 +4,14 @@
 #SBATCH --time=0-72:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=def-ashique
-#SBATCH --array=1-250
 
-module load python/3.10
 source $HOME/Documents/ENV/bin/activate
+module load python/3.10
 module load mujoco mpi4py
 
 SECONDS=0
-#python Reacher_weighted.py --seed $SLURM_ARRAY_TASK_ID  --type "catch" --env "ball_in_cup" --log_dir $SCRATCH/avg_discount/dm_control/ --epochs 500&
-python Reacher_weighted.py --seed $SLURM_ARRAY_TASK_ID  --type "easy" --env "point_mass" --log_dir $SCRATCH/avg_discount/dm_control/ --epochs 500&
+python Reacher_weighted.py --seed $SLURM_ARRAY_TASK_ID  --type "catch" --env "ball_in_cup" --log_dir $SCRATCH/avg_discount/dm_diff/ --epochs 500&
+python Reacher_weighted.py --seed $SLURM_ARRAY_TASK_ID  --type "easy" --env "point_mass" --log_dir $SCRATCH/avg_discount/dm_diff/ --epochs 500&
 
 echo "Baseline job $seed took $SECONDS"
 sleep 72h
